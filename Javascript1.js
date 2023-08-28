8bcfcac0c100f45c3a9b9a7e7141d6186e2edd61
Rafif Azzaki <rafif.azzaki@zettacamp.pro>
2023-08-28 08:49:34 +0700

    file transfer

---
+++ Javascript1.js
@@ -0,0 +1,40 @@
+let favourite_book = "Sherlock Holmes";
+const fav_book = "Keep Going";
+let number = "15"
+const bool = false;
+
+
+
+favourite_book="Everlost"
+
+// #1.a
+console.log(favourite_book);
+
+// #1.b
+
+
+try {
+    fav_book="Steal_like_an_artist";
+    console.log(fav_book);
+  }
+  catch(err) {
+    console.log(fav_book);
+  }
+
+
+// #2.concat dengan dynamic variabel
+
+for (var i = 0; i <= 4; i++) {
+    eval(favourite_book+ "_"+ i + " = " + "\"test\"");
+  }
+
+console.log(Everlost_1)
+console.log(Everlost_2)
+
+// concat dengan dynamic variable dan typeof 
+Everlost_5= typeof(bool) + " " + fav_book + " " + Everlost_1 + Everlost_3;
+console.log(Everlost_5);
+
+Everlost_4=Everlost_1 + Everlost_3 + +number;
+console.log(Everlost_4);
+
