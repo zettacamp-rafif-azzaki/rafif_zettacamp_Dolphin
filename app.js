a639fd586df15b97848b085d57016215b54a4bb9
Rafif Azzaki <rafif.azzaki@zettacamp.pro>
2023-09-18 08:50:58 +0700

    update app.js with mongodb

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
 let port = 3003;
 
 
-app.post('/post',(req, res) => {
+app.post('/post', connect(),(req, res) => {
     
     // res.status(401).send(a);
   });
 
+async function connect(){
+    const url = 'mongodb://0.0.0.0:27017/';
+    const database = 'zettacamp_BookPurchase';
 
+    mongoose.connect(`${url}${database}`,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    }).then(() => {
+        console.log('connected to MongoDB');
+    }).catch((error) => {
+        console.log('Error connecting to MongoDB: ', error);
+    });
+}
 
 
 
